cf6333ff7b0a2fb89044a503154299176a2a1721
Sakshi Mishra <Sakshimishr0908@gmail.com>
2025-08-21 11:51:20 +0530

    Added full project files

---
+++ index.js
@@ -0,0 +1,11 @@
+const express=require('express');
+const app=exp();
+const mongoose=require('mangoose');
+const cors=require('cors');
+
+app.use(cors());
+app.use(exp.json());
+
+app.get('/',(req,res) =>{
+  
+}
